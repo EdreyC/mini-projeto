625d77dc5e72421ed55ca21a51a26f1b7fe95c4e
EdreyC <edreycarneiro.profissional@gmail.com>
2022-04-25 16:35:04 -0300

    update

--- test/register-user-on-mailing-list.spec.ts
+++ test/register-user-on-mailing-list.spec.ts
@@ -1,21 +1,49 @@
 import { RegisterUserOnMailingList } from "../src/register-user-on-mailing-list"
 import { UserRepository } from "../src/user-repository";
 
+describe('EmailList', () => {
 
-describe("Register user on mailing list",()=>{
+    let registerUserOnMailingList: RegisterUserOnMailingList
+    let userRepository: UserRepository
 
-    const RegisterUserOnMainlingList = new RegisterUserOnMailingList();
-    const Repository = new UserRepository()
-
-    test("Should return null if the email already exists",()=>{
-
-        const Data ={
-            name:"Edrey",
-            email:"edrey@gmail.com"
-        }
-        Repository.add(Data)
-        const response = RegisterUserOnMainlingList.execute(Data)
-        
-        expect(response).toBe(null)
+    beforeEach(function () {
+        userRepository = new UserRepository();
+        registerUserOnMailingList = new RegisterUserOnMailingList(userRepository)
     })
+
+    it("Should return an error if e-mail already in use", () => {
+
+        const findBy = jest.spyOn(userRepository, 'findBy')
+        findBy.mockReturnValue(
+            {
+                name: "Edrey",
+                email: "edrey@gmail.com"
+            }
+        )
+        expect(() => {
+            registerUserOnMailingList.execute({
+
+                name: "Edrey",
+                email: "edrey@gmail.com"
+            })
+        }).toThrow("User already registered")
+    })
+
+
+    //2º Case: User already registred - False
+    it("Should return an error if user not already registred not the system", () => {
+        const spy = jest.spyOn(userRepository, 'add')
+        spy.mockReturnValue(false)
+        expect(() => {
+            registerUserOnMailingList.execute({
+                name: 'Marcos',
+                email: "marcos@gmail.com"
+            })
+        }).toThrow("User not created on database")
+    })
+
+    //3º Case: Don't send confirmation e-mail
+
+
+
 })
